d9dee287df00f3cf7adb01d3bb818d2cdb06b132
Emigham7 <emigham7@gmail.com>
2023-03-14 21:39:28 +0000

    coding is fun

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-export.nbOccurences = function (list, searchElement) {
-  return list.ruduce((count, item)) =>
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, item) =>
     (item === searchElement ? count + 1 : count), 0
-  }
-}; 
+  );
+};
